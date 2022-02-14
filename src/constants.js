ace026cc6dfc2a54a9b2c429bef3f07509f62ac1
Hyblocker <bryan.spiteri@gmail.com>
2022-02-15 00:50:37 +0100

    update urls

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@ export const SITE_NAME = "Augmented Hip";
 export const DOMAIN = "getaughip.com";
 
 // AUGHIP URLS
-export const LATEST_INSTALLER = "https://github.com/hyblocker/aughip-installer-gui/releases/download/0.1/aughip-installer-gui.exe";
+// export const LATEST_INSTALLER = "https://github.com/aughip/aughip-installer-gui/releases/download/0.1/aughip-installer-gui.exe";
+export const LATEST_INSTALLER = "https://download.getaughip.com";
 export const DISCORD_INVITE = "https://discord.gg/YBQCRDG";
 export const GITHUB_REPO = "https://github.com/aughip";
-export const GITHUB_RELEASES = "https://github.com/hyblocker/augmented-hip/releases";
+export const GITHUB_RELEASES = "https://github.com/aughip/augmented-hip/releases";
